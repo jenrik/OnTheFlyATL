debe89de0760567c849a5f2a42b53d49818ec83f
Jener Rasmussen <jener@jener.dk>
2021-04-06 22:41:32 +0200

    Port UI code from javascript to rust

--- web/js/index.js
+++ web/js/index.js
@@ -1,11 +1,5 @@
 import("../pkg/index.js")
     .catch(console.error)
     .then(module => {
-        document.getElementById("solve").onclick =
-            (_) => {
-                let model = document.getElementById("lcgs-model").value;
-                let formula = document.getElementById("atl-formula").value;
-
-                document.getElementById("result").innerText = module.check(model, formula);
-            };
+        document.atl_checker = module;
     });
--- web/src/lib.rs
+++ web/src/lib.rs
@@ -14,7 +14,8 @@ use atl_checker::search_strategy::SearchStrategyBuilder;
 use std::error::Error;
 use std::sync::Arc;
 use wasm_bindgen::prelude::*;
-use web_sys::console;
+use wasm_bindgen::JsCast;
+use web_sys::{console, HtmlButtonElement, HtmlDivElement, HtmlInputElement, HtmlTextAreaElement};
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -34,45 +35,43 @@ pub fn main_js() -> Result<(), JsValue> {
 
     tracing_wasm::set_as_global_default();
 
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+
+    let elem_lcgs = document
+        .get_element_by_id("lcgs-model")
+        .unwrap()
+        .dyn_into::<HtmlTextAreaElement>()
+        .unwrap();
+    let elem_atl = document
+        .get_element_by_id("atl-formula")
+        .unwrap()
+        .dyn_into::<HtmlInputElement>()
+        .unwrap();
+    let elem_submit = document
+        .get_element_by_id("solve")
+        .unwrap()
+        .dyn_into::<HtmlButtonElement>()
+        .unwrap();
+    let elem_result = document
+        .get_element_by_id("result")
+        .unwrap()
+        .dyn_into::<HtmlDivElement>()
+        .unwrap();
+
+    let a = Closure::wrap(Box::new(move || {
+        let lcgs: String = elem_lcgs.value().to_string();
+        let atl: String = elem_atl.value().to_string();
+        let result = check_model(lcgs.as_str(), atl.as_str());
+        elem_result.set_inner_html(format!("{:?}", result.unwrap()).as_str());
+    }) as Box<dyn FnMut() + 'static>);
+    elem_submit.set_onclick(Some(a.as_ref().unchecked_ref()));
+    a.forget();
+
     Ok(())
 }
 
-#[wasm_bindgen]
-pub fn numbers(model: &str) {
-    let lcgs = parse_lcgs(model)
-        .map_err(|err| format!("Failed to parse the LCGS program.\n{}", err))
-        .unwrap();
-
-    let ir = IntermediateLCGS::create(lcgs)
-        .map_err(|err| format!("Invalid LCGS program.\n{}", err))
-        .unwrap();
-
-    console::log_1(&JsValue::from_str("Players:"));
-    for player in &ir.get_player() {
-        console::log_1(&JsValue::from_str(
-            format!("{} : {}", player.get_name(), player.index()).as_str(),
-        ));
-    }
-
-    console::log_1(&JsValue::from_str("\nLabels:"));
-    for label_symbol in &ir.get_labels() {
-        let label_decl = ir.get_decl(&label_symbol).unwrap();
-        if let DeclKind::Label(label) = &label_decl.kind {
-            if Owner::Global == label_symbol.owner {
-                console::log_1(&JsValue::from_str(
-                    format!("{} : {}", &label_symbol.name, label.index).as_str(),
-                ));
-            } else {
-                console::log_1(&JsValue::from_str(
-                    format!("{} : {}", &label_symbol, label.index).as_str(),
-                ));
-            }
-        }
-    }
-}
-
-#[wasm_bindgen]
-pub fn check(lcgs_model: &str, atl_formula: &str) -> Result<JsValue, JsValue> {
+fn check_model(lcgs_model: &str, atl_formula: &str) -> Result<VertexAssignment, Box<dyn Error>> {
     let lcgs = parse_lcgs(&lcgs_model)
         .map_err(|err| format!("Failed to parse the LCGS program.\n{}", err))?;
 
@@ -103,6 +102,6 @@ pub fn check(lcgs_model: &str, atl_formula: &str) -> Result<JsValue, JsValue> {
 
     match broker.get_result() {
         None => panic!("worker terminated without setting result"),
-        Some(result) => Ok(JsValue::from(result.to_string())),
+        Some(result) => Ok(result),
     }
 }
